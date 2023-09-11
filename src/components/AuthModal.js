d2c21d3a182a9809beff1fd312a77d13d9b99429
Ganesh <ganesh.suthraya@gmail.com>
2023-09-11 19:52:02 +0530

    Change backend url

--- src/components/AuthModal.js
+++ src/components/AuthModal.js
@@ -28,7 +28,9 @@ const AuthModal = ({ setShowModal, isSignUp }) => {
       }
 
       const response = await axios.post(
-        `http://localhost:8000/${isSignUp ? "signup" : "login"}`,
+        `https://fair-gray-dragonfly-yoke.cyclic.app/${
+          isSignUp ? "signup" : "login"
+        }`,
         { email, password }
       );
 
